d3510b9f5b88f0f587fbddb8bfffa0885f7db15b
Bolotov Alexander <distalker1@gmail.com>
2022-01-18 08:51:01 +0300

    pom update

--- src/main/java/koschei/KoscheiTheDeathless.java
+++ src/main/java/koschei/KoscheiTheDeathless.java
@@ -10,11 +10,14 @@ public class KoscheiTheDeathless {
     private Ocean1 ocean;
 
     public String getRulesByDeth() {
-        return "На свете есть океан , " + ocean.toString();
+        return "На свете есть океан, " + ocean.toString();
     }
 
     @Autowired
     public void setOcean(Ocean1 ocean) {
         this.ocean = ocean;
     }
+
+
+
 }
